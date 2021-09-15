52e7c0625d9879137a7d50dc5cdcf7d95317ace7
AbdurRahman-netizen <c163022@ugrad.iiuc.ac.bd>
2021-09-15 23:18:33 +0600

    added menuBar

--- index.php
+++ index.php
@@ -8,20 +8,20 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/styl.css">
     <title>HNSMS</title>
 </head>
 
 <body>
 
     <nav class="navbar navbar-expand-sm navbar-dark bg-info pl-5 fixed-top">
-        <a href="index.php" class="navbar-brand"><img src="images/HNS (2).png" class="rounded-circle" alt="" style="height: 50px;">NMS</a>
+        <a href="index.php" class="navbar-brand "><img src="images/HNS (2).png" class="rounded-circle mb-3" alt="" style="height:50px; "><span class="brand">NMS</span> </a>
         <span class="navbar-text">"stay home we will take care"</span>
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#myMenu">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="myMenu">
-            <ul class="navbar-nav pl-5">
+            <ul class="navbar-nav pl-5  ml-5 navItemStyle">
                 <li class="nav-link"><a href="" class="nav-item">Home</a></li>
                 <li class="nav-link"><a href="" class="nav-item">Our Services</a></li>
                 <li class="nav-link"><a href="" class="nav-item">Login</a></li>
