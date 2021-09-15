7cb33b27d29b5dd2268c8b44865e2b5d33a196f5
AbdurRahman-netizen <c163022@ugrad.iiuc.ac.bd>
2021-09-15 20:37:16 +0600

    add google font

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/all.min.css">
-    <link rel="stylesheet" href="css/custom.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
     <title>HNSMS</title>
 </head>
 
