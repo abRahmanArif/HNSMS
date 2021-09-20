d8526d3044c364ccb08669078dd22f71306e605e
AbdurRahman-netizen <c163022@ugrad.iiuc.ac.bd>
2021-09-20 23:25:45 +0600

    added Services & AboutUs section

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/styl.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>HNSMS</title>
 </head>
 
@@ -24,12 +24,109 @@
             <ul class="navbar-nav pl-5  ml-5 navItemStyle">
                 <li class="nav-link"><a href="" class="nav-item">Home</a></li>
                 <li class="nav-link"><a href="" class="nav-item">Our Services</a></li>
+                <li class="nav-link"><a href="" class="nav-item">About Us</a></li>
                 <li class="nav-link"><a href="" class="nav-item">Login</a></li>
                 <li class="nav-link"><a href="" class="nav-item">Contact</a></li>
             </ul>
         </div>
     </nav>
+    <!-- start bannerSection -->
+    <div class="container-flude">
+        <div class="jumbotron bannerSizing" style="background-image: url(images/nurse.jpg);">
+            <div class="mainHeader text-primary font-weight-bold text-center">
+                <h1 class="">Welcome</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus autem modi laboriosam.</p>
+                <a href="#" class="btn btn-outline-primary">SignUp</a>
+            </div>
+        </div>
+    </div>
+    <!-- End bannerSection -->
+    <!-- Start content -->
+    <div class="container rounded bg-info p-5">
+        <h1 class="text-center pb-5 text-white">Our Services</h1>
+        <div class="row">
+            <div class="col-md-4 ">
+                <div class="card  text-center">
+                    <img class="card-img-top shadow-lg imgSizing" src="images/newborn1.jpg" alt="">
+                    <div class="card-body">
+                        <h3 class="card-title  cimg">NewBorn Baby Care</h3>
+                        <hr>
+                        <p class="card-text mt-2">Lorem ipsum dolor sit, amet consectetur adipisicing....</p>
+                        <a href="#" class="btn btn-primary">See More...</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 ">
+                <div class="card  text-center">
+                    <img class="card-img-top shadow-lg imgSizing" src="images/baby.jpg" alt="">
+                    <div class="card-body">
+                        <h3 class="card-title childimg">Child Baby Care</h3>
+                        <hr>
+                        <p class="card-text mt-2">Lorem ipsum dolor sit, amet consectetur adipisici ng Lorem ipsum dolor....</p>
+                        <a href="#" class="btn btn-primary">See More...</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card  text-center">
+                    <img class="card-img-top shadow-lg imgSizing" src="images/baby1.jpg" alt="">
+                    <div class="card-body">
+                        <h3 class="card-title  cimg">NewBorn Baby Care</h3>
+                        <hr>
+                        <p class="card-text mt-2">Lorem ipsum dolor sit, amet consectetur adipisicing....</p>
+                        <a href="#" class="btn btn-primary">See More...</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-4">
+                <div class="card  text-center">
+                    <img class="card-img-top shadow-lg imgSizing" src="images/newborn1.jpg" alt="">
+                    <div class="card-body">
+                        <h3 class="card-title  cimg">NewBorn Baby Care</h3>
+                        <hr>
+                        <p class="card-text mt-2">Lorem ipsum dolor sit, amet consectetur adipisicing....</p>
+                        <a href="#" class="btn btn-primary">See More...</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 ">
+                <div class="card  text-center">
+                    <img class="card-img-top shadow-lg imgSizing" src="images/newborn1.jpg" alt="">
+                    <div class="card-body">
+                        <h3 class="card-title  cimg">NewBorn Baby Care</h3>
+                        <hr>
+                        <p class="card-text mt-2">Lorem ipsum dolor sit, amet consectetur adipisicing....</p>
+                        <a href="#" class="btn btn-primary">See More...</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card  text-center">
+                    <img class="card-img-top shadow-lg imgSizing" src="images/newborn1.jpg" alt="">
+                    <div class="card-body">
+                        <h3 class="card-title  cimg">NewBorn Baby Care</h3>
+                        <hr>
+                        <p class="card-text mt-2">Lorem ipsum dolor sit, amet consectetur adipisicing....</p>
+                        <a href="#" class="btn btn-primary">See More...</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <div class="container-flued mt-3">
+        <div class="jumbotron  pt-4 pb-4">
+            <h1 class="text-center text-info font-weight-bold">About Us</h1>
+            <div class="row">
+                <div class="col text-justify">
+                    <p class="aboutStyle"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, accusantium. Vitae itaque deserunt similique dolores quaerat omnis non debitis! Ea, beatae natus sed saepe, odio quos qui, dolore harum cumque dolorum facilis accusamus modi! Recusandae, fugit officia aperiam maiores assumenda ipsam, facilis cupiditate at eos dolores, consequuntur porro odio doloribus! Recusandae nisi iure voluptatibus ex quidem amet fugit quis laudantium veritatis aspernatur inventore, esse id blanditiis officiis ratione. Expedita provident dolorum at! Inventore modi dolores quod quo! Id suscipit cumque enim, illo, doloribus eaque aspernatur, consequuntur quas dolorum esse laudantium! At hic, aliquid facilis consequatur exercitationem explicabo quisquam eaque ullam nisi doloribus cumque voluptatibus rem eligendi eius quia iusto recusandae aspernatur. Velit suscipit inventore ut dolore, dolorum culpa quae doloribus deleniti nobis asperiores minima, dicta enim ipsam! Accusantium odio dolores porro est? Laborum, dolores. Sed magnam recusandae accusamus nisi reiciendis et? Provident, neque eveniet. Laudantium rem illo voluptatem sit nulla, corrupti laboriosam consequatur quas, aspernatur doloribus officiis fuga voluptatum dicta eligendi vitae dolor nemo hic, incidunt voluptas minima suscipit molestiae accusamus aliquid odit. Impedit tempore eius incidunt quod assumenda tempora, accusantium rerum repellendus vitae nihil nobis animi praesentium voluptatibus soluta eum expedita, molestiae error. Earum, sequi? Cupiditate maxime amet at!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br><br>
 
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
